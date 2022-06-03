b25b42531a279e62dfda0d3239c6e26a1f1aec11
Yahya <yahyahussein670@gmail.com>
2022-06-03 17:14:03 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    alert ('I was clicked!');
+    input.addEventListener('click', addingEventListener);
 }
+
+
+
+
+
+/*
+const input = document.getElementById('input');
+
+
+function clickAlert() {
+    alert ('I was clicked!')
+}
+input.addEventListener('click', clickAlert);
+
+*/
